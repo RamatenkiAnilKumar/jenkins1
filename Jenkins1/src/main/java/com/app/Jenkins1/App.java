b355ef01e164f20d8f77b7459edd54b07259dace
chaan <chaan@DESKTOP-68U3HT9>
2020-03-15 05:49:36 -0700

    jenkins1 1st commited

---
+++ Jenkins1/src/main/java/com/app/Jenkins1/App.java
@@ -0,0 +1,16 @@
+package com.app.Jenkins1;
+
+/**
+ * Hello world!
+ *
+ */
+ class App 
+{
+    public static void main( String[] args )
+    {
+    	Test t1=new Test();
+    	t1.m1();
+        System.out.println( "Hello World!" );
+        System.out.println( "Jenkins Trails " );
+    }
+}
---
+++ Jenkins1/src/main/java/com/app/Jenkins1/Test.java
@@ -0,0 +1,10 @@
+package com.app.Jenkins1;
+
+public class Test {
+
+	
+	public void m1() {
+		
+		System.out.println("m1 calling");
+	}
+}
